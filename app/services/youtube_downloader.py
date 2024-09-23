8a267b608e6a1646042f9a4997ed63c9a13ce4bd
Ywoosang <opellong13@gmail.com>
2024-09-24 02:39:50 +0900

    feat: 이미 존재하는 영상의 경우 다운로드하지 않고 건너뛰도록 함

--- app/services/youtube_downloader.py
+++ app/services/youtube_downloader.py
@@ -1,19 +1,26 @@
 from pytubefix import YouTube
 from pytubefix.cli import on_progress
 
+import os
 
 def download_video(youtube_url: str, download_path: str = "downloads/") -> str:
     yt = YouTube(youtube_url.strip(), on_progress_callback = on_progress)
-    # 비디오 제목
-    print(yt.title)
 
-    # 저장될 파일 이름 = 유투브 비디오 ID
+    video_title = yt.title
     video_id = yt.video_id
-    filename = f"{video_id}.mp4"
 
+    print("비디오 다운로드 시작")
+    print(f"비디오 제목: {video_title}")
+    print(f"비디오 ID: {video_id}")
+
+    filename = f"{video_id}.mp4"
+    file_path = os.path.join(download_path, filename)
+    
     ys = yt.streams.get_highest_resolution()   
 
-    ys.download(output_path= download_path, filename= filename)
-    file_path = f"{download_path}/{filename}"
-
+    if os.path.exists(file_path):
+        print(f"비디오가 이미 다운로드 폴더에 존재: {video_title}")
+    else:
+        ys.download(output_path= download_path, filename= filename)
+    print("비디오 다운로드 완료")
     return file_path
