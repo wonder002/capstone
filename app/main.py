7c8eedc91b1b65242b8a5c2e937ca29fe79e9ef2
Ywoosang <opellong13@gmail.com>
2024-09-23 22:04:30 +0900

    initial commit

---
+++ app/main.py
@@ -0,0 +1,21 @@
+from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+origins = [
+    "http://127.0.0.1:8080",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/video")
+def hello():
+
+    return {"message": "테스트"}
